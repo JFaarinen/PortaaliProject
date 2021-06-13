535c0fddbae09ddd7d235aa2086cc4c899cf4286
Juho F <juho.faarinen@gmail.com>
2021-06-14 00:17:03 +0300

    Lisätty get/get:id/post/delete routet

--- portaali-backend/index.js
+++ portaali-backend/index.js
@@ -13,136 +13,6 @@ const requestLogger = (request, response, next) => {
     next();
 }
 
-let tuotteet = [
-    {
-        id: 1,
-        nimi: "Haarukka",
-        kategoriat: [
-            "Astiat",
-            "Ruoanlaitto",
-            "Aterimet"
-        ],
-        hinta: 1.50,
-        lkm: 13,
-        kuvaus: "Siinä on piikkejä varren päässä mutta heinähankoa pienempi...",
-        img: "./images/item-1.jpg"
-    },
-    {
-        id: 2,
-        nimi: "Kuppi, savea",
-        kategoriat: [
-            "Astiat",
-            "Ruoanlaitto",
-            "Kupit"
-        ],
-        hinta: 2.00,
-        lkm: 20,
-        kuvaus: "Savikuppeja, eri näköisiä",
-        img: "./images/item-2.jpg"
-    },
-    {
-        id: 3,
-        nimi: "Styroksilaatikko, iso",
-        kategoriat: [
-            "Labra",
-            "Lääkintä",
-            "Säilytys"
-        ],
-        hinta: 1.50,
-        lkm: 2,
-        kuvaus: "Laatikko. Siihen menee sisälle tavaraa. Tai kissa",
-        img: "./images/item-3.jpg"
-    },
-    {
-        id: 4,
-        nimi: "Dekantterilasi",
-        kategoriat: [
-            "Labra",
-            "Lääkintä"
-        ],
-        hinta: 3.00,
-        lkm: 7,
-        kuvaus: "Dekantterilasi, eri kokoja. Myöhemmin päivitellään ehkä valintoja per koko",
-        img: "./images/item-4.jpg"
-    },
-    {
-        id: 5,
-        nimi: "Röntgenkuva, aivot",
-        kategoriat: [
-            "Labra",
-            "Lääkintä",
-            "Kauhu"
-        ],
-        hinta: 12.00,
-        lkm: 1,
-        kuvaus: "Igor kaivoi tän jostain. A.B Normal",
-        img: "./images/item-5.jpg"
-    },
-    {
-        id: 6,
-        nimi: "Diskovalo, pöytämalli",
-        kategoriat: [
-            "Valaistus",
-            "Tehosteet"
-        ],
-        hinta: 10.00,
-        lkm: 1,
-        kuvaus: "Partypallo suoraan 70 -luvulta",
-        img: "./images/item-6.jpg"
-    },
-    {
-        id: 7,
-        nimi: "Pissauspullo",
-        kategoriat: [
-            "Labra",
-            "Lääkintä",
-            "Squik!"
-        ],
-        hinta: 0.50,
-        lkm: 1,
-        kuvaus: "Pullo pissalle. Sisällön saa tuottaa itse. Palautettava pestynä",
-        img: "./images/item-7.jpg"
-    },
-    {
-        id: 8,
-        nimi: "Lyhty",
-        kategoriat: [
-            "Valaistus",
-            "Fantasia",
-            "Keskiaika"
-        ],
-        hinta: 6.00,
-        lkm: 8,
-        kuvaus: "Lyhty. Pannaan tuli sisään, tulee valoa ulos",
-        img: "./images/item-8.jpg"
-    },
-    {
-        id: 9,
-        nimi: "Kaasunaamari",
-        kategoriat: [
-            "Post-apo",
-            "Suojavaruste"
-        ],
-        lkm: 21,
-        hinta: 5.00,
-        kuvaus: "Nasseja. Kumia. Ei suojaa kaasulta oikeasti.",
-        img: "./images/item-9.jpg"
-    },
-    {
-        id: 10,
-        nimi: "Lääkärintakki",
-        kategoriat: [
-            "Vaatetus",
-            "Labra",
-            "Lääkintä"
-        ],
-        hinta: 6.50,
-        lkm: 8,
-        kuvaus: "Lääkärintakkeja, eri kokoja.",
-        img: "./images/item-10.jpg"
-    }
-];
-
 app.use(express.json());
 app.use(express.static('build'));
 app.use(cors());
@@ -158,27 +28,29 @@ app.get('/api/tuotteet', (req, res) => {
         .then(tuotteet => { res.json(tuotteet) });
 });
 
-app.get('/api/tuotteet/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const tuote = tuotteet.find(t => t.id === id);
-
-    if (tuote) {
-        res.json(tuote);
-    } else {
-        res.status(404).end();
-    }
+app.get('/api/tuotteet/:id', (req, res, next) => {
+    const id = req.params.id;
+    Tuote.findById(id)
+        .then(tuote => {
+            if (tuote) {
+                res.json(tuote);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
-app.delete('/api/tuotteet/:id', (req, res) => {
-    const id = Number(req.params.id);
-    tuotteet = tuotteet.filter(t => t.id !== id);
-    res.status(204).end();
+app.delete('/api/tuotteet/:id', (req, res, next) => {
+    const id = req.params.id;
+    Tuote.findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.post('/api/tuotteet', (req, res) => {
-    const maxId = tuotteet.length > 0
-        ? Math.max(...tuotteet.map(t => t.id)) : 0;
-
     const body = req.body;
 
     if (!body.nimi) {
@@ -187,18 +59,18 @@ app.post('/api/tuotteet', (req, res) => {
         });
     }
 
-    const tuote = {
+    const tuote = new Tuote({
         nimi: body.nimi,
         kategoriat: body.kategoriat,
         hinta: body.hinta || 0.00,
         lkm: body.lkm || 1,
         kuvaus: body.kuvaus,
-        img: body.img,
-        id: maxId + 1
-    }
+        img: body.img
+    });
 
-    tuotteet = tuotteet.concat(tuote);
-    res.json(tuote);
+    tuote.save().then(lisattyTuote => {
+        res.json(lisattyTuote)
+    });
 });
 
 const unknownEndpoint = (req, res) => {
@@ -206,6 +78,17 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' });
+    }
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
