1893da93f7c49bd2fb8aebd6d2cd61e9a36c9e83
Juho F <juho.faarinen@gmail.com>
2021-07-31 22:46:00 +0300

    tuoteformin ulkoasun muokkausta

--- portaali-frontend/src/components/TuoteTiedotForm.js
+++ portaali-frontend/src/components/TuoteTiedotForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Grid, TextField, Button } from '@material-ui/core';
 
 const TuoteTiedotForm = ({ tuoteTiedot, setTuoteTiedot }) => {
     const [values, setValues] = useState({ tuote: '', hinta: 0, lkm: 1 });
@@ -13,34 +14,45 @@ const TuoteTiedotForm = ({ tuoteTiedot, setTuoteTiedot }) => {
         }
     }
     return (
-        <div>
+        <Grid container alignItems='center' spacing={2}>
             <form onSubmit={handleSubmit}>
-                <div>
-                    Tuote: <input
+                <Grid item sx={12} sm={12} md={12} lg={12}>
+                    <TextField
+                        label='Tuotteen tiedot'
+                        placeholder='Esim: Labratakki, valkoinen, koko s'
+                        variant='outlined'
                         name='tuote'
                         type='text'
                         value={values.tuote}
                         onChange={(event) => setValues({ ...values, tuote: event.target.value })}
                     />
-                </div>
-                Hinta: <input
-                    name='hinta'
-                    type='text'
-                    value={values.hinta}
-                    onChange={(event) => setValues({ ...values, hinta: event.target.value })}
-                />
-                Lkm: <input
-                    name='lkm'
-                    type='number'
-                    value={values.lkm}
-                    onChange={(event) => setValues({ ...values, lkm: event.target.value })}
-                />
+                </Grid>
+                <Grid item sx={12} sm={6} md={6}>
+                    <TextField
+                        label='Hinta'
+                        variant='outlined'
+                        name='hinta'
+                        type='text'
+                        value={values.hinta}
+                        onChange={(event) => setValues({ ...values, hinta: event.target.value })}
+                    />
+                </Grid>
+                <Grid item sx={12} sm={6} md={6}>
+                    <TextField
+                        label='Lkm'
+                        variant='outlined'
+                        name='lkm'
+                        type='number'
+                        value={values.lkm}
+                        onChange={(event) => setValues({ ...values, lkm: event.target.value })}
+                    />
+                </Grid>
                 <button type='submit'>Lisää tuote</button>
             </form>
             <ul>
                 {tuoteTiedot.map((t) => <li key={t.tuote}>{t.tuote}</li>)}
             </ul>
-        </div>
+        </Grid>
     );
 }
 
