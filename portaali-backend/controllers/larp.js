10b9bcfe26e71aa10dd3342eeb1fcea011256135
Juho Faarinen <juho.faarinen@gmail.com>
2022-01-10 20:47:19 +0200

    lisätty router pelien noutamiselle

--- portaali-backend/controllers/larp.js
+++ portaali-backend/controllers/larp.js
@@ -1,1 +1,21 @@
 //kontrolleri peliesittelyiden lisäämiselle
+const larpRouter = require('express').Router();
+const Larp = require('../models/larp');
+
+larpRouter.get('/', async (req, res) => {
+    const larpit = await Larp.find({});
+    res.json(larpit);
+});
+
+larpRouter.get('/:id', async (req, res, next) => {
+    const larp = await Larp.findById(req.params.id);
+    console.log('larp noudettu: ', larp);
+    if (larp) {
+        res.json(larp.toJSON());
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = larpRouter;
+
